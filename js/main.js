018e6da2e65cee5b19c831b044cc438167f98db0
Chris Thorp <info@christhorp.hk>
2014-03-31 21:23:17 +0800

    Website Build

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
 $(document).ready(function(){
    $("#outer-wrap").onepage_scroll(); 
+   
+   site.resize();
+
+    $(window).resize(function(){
+        site.resize();
+    });
+    
+    $(window).load(function(){
+        site.resize();
+    });
+    
+    $('.portfolio').cycle({
+        slides: '.port-slide'
+    });
+   
+   
+   
 });
+
+
+
+var site = {
+    resize: function(){
+        var new_margin = Math.ceil(($(window).height() ) / 2);
+        $('.vert-align').each(function(){
+            $(this).css('margin-top', new_margin + 'px');
+            $(this).css('top',-($(this).outerHeight() / 2) - 50);
+        });
+    }
+};
